e6425b2d123e19de5c4fc1e1d8342dd159cf3e47
CarQuispe <carmen.quispe@ucb.edu.bo>
2024-12-13 22:27:14 -0400

    final

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,35 @@
-import sumar from "./sumador";
+import sumar from "./sumador.js";
+import restar from "./restador.js";
+import multiplicar from "./multiplicador.js";
+import dividir from "./divisor.js";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const operation = document.querySelector("#operacion"); // Un `select` para elegir la operación
+const form = document.querySelector("#calcular-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = Number.parseFloat(first.value);
+  const secondNumber = Number.parseFloat(second.value);
+  const selectedOperation = operation.value;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  let result;
+  try {
+    switch (selectedOperation) {
+      case "sumar":
+        result = sumar(firstNumber, secondNumber);
+        break;
+      case "restar":
+        result = restar(firstNumber, secondNumber);
+        break;
+            default:
+        result = "Operación no válida";
+    }
+    div.innerHTML = `<p>Resultado: ${result}</p>`;
+  } catch (error) {
+    div.innerHTML = `<p>Error: ${error.message}</p>`;
+  }
 });
---
+++ src/restador.tes.js
@@ -0,0 +1,9 @@
+import restar from "./restador.js";
+
+describe("Restar", () => {
+  it("debería restar dos números", () => {
+    expect(restar(5, 2)).toEqual(1);
+  });
+
+  
+});
